9013e81bed1976d79cd38b8df3243d4295f5c4fc
watan beck <96546018+steak137@users.noreply.github.com>
2024-01-24 17:21:30 +0300

    char missing
    bored

--- leet_translator.cpp
+++ leet_translator.cpp
@@ -7,38 +7,53 @@ std::map<char, char> char_map =
     {'a', '4'},
     {'A', '4'},
 
+    {'b', '8'},
+    {'B', '8'},
+
     {'e', '3'},
     {'E', '3'},
 
-    {'s', '5'},
-    {'S', '5'},
+    {'g', '9'},
+    {'G', '9'},
+
+    {'r', '2'},
+    {'R', '2'},
+
+    {'i', '1'},
+    {'I', '1'},
+
+    {'l', '1'},
+    {'L', '1'},
 
     {'o', '0'},
     {'O', '0'},
 
+    {'s', '5'},
+    {'S', '5'},
+
     {'t', '7'},
     {'T', '7'},
 
-    {'l', '1'},
-    {'L', '1'}
+    {'z', '2'},
+    {'Z', '2'}
 };
 
 auto main() -> int {
     std::string str;
     std::cout << "Write your text\n";
-LOOPSTART:;
-    std::cin >> str;
-    std::string result;
-    for (auto& c : str) {
-        if (char_map.find(c) != char_map.end()) {
-            result.push_back(char_map[c]);
+    while (1) {
+        std::cin >> str;
+        std::string result;
+        for (auto& c : str) {
+            if (char_map.find(c) != char_map.end()) {
+                result.push_back(char_map[c]);
+            }
+            else {
+                result.push_back(c);
+            }
         }
-        else {
-            result.push_back(c);
-        }
+        std::cout << result << '\n';
+        str = "";
+        result = "";
     }
-    std::cout << result << '\n';
-    str = "";
-    result = "";
-    goto LOOPSTART;
 }
